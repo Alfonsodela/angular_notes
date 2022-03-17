47ad26336d125a40445fd563f0a785b8519cfc2b
Alfonsodela <89443967+Alfonsodela@users.noreply.github.com>
2022-03-17 22:09:47 +0100

    Grande Jaime

--- client/src/app/shared/shared.module.ts
+++ client/src/app/shared/shared.module.ts
@@ -4,7 +4,6 @@ import { CommonModule } from '@angular/common';
 
 import { ReactiveFormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
-import { AppRoutingModule } from '../app-routing.module';
 import { NoteComponent } from './components/note/note.component';
 
 
@@ -18,7 +17,6 @@ import { NoteComponent } from './components/note/note.component';
     CommonModule,
     ReactiveFormsModule,
     RouterModule,
-    AppRoutingModule
   ],
   exports: [
     NoteComponent,
